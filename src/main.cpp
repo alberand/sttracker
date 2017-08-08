8ae920a4fc84c21559930aa17a31af544030d58d
alberand <albershteyn.andrey@gmail.com>
2017-08-08 11:15:35 +0200

    src: Add libs and WIFION mode to disable wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include "mbed.h"
 
+// MBed libs
 #include "TCPSocket.h"
 #include "SpwfInterface.h"
+#include "MyBuffer.h"
+#include "BufferedSerial.h"
 
+// Custom libs
 #include "utils.h"
+#include "minmea.h"
 
-//#define DEBUG 0
 
 // "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A"
 // 1,1,20170717100030.000,50.080988,14.388322,82.200,
 
+#define WIFION 0
+//#define DEBUG 0
 #ifdef DEBUG
     #define DEBUG(x) pc.printf x
 #else
@@ -22,8 +28,8 @@
 
 // Init serial with PC
 Serial pc(USBTX, USBRX);
-// Init serial with SIM808_V2, Serial3 for STM32 Nucleo L476G
-Serial module(A0, A1);
+// Init serial with SIM808_V2, Serial4 for STM32 Nucleo L476G
+BufferedSerial module(A0, A1);
 
 // Informing LED
 DigitalOut green(LED1);
@@ -34,8 +40,7 @@ DigitalIn mybutton(USER_BUTTON);
 SpwfSAInterface spwf(D8, D2, false);
 
 // Global response buffer
-int buf_ind = 0;
-char buffer[BUFFER_SIZE];
+MyBuffer <char> sim808_buffer;
 
 /* @brief Interrupt handler stores bytes received from SIM808 module into global
  * buffer.
@@ -44,57 +49,18 @@ void SIM808_V2_IRQHandler(void)
 {    
     char ch = module.getc();
 
-    buffer[buf_ind] = ch;
-    buf_ind++; 
-} 
+    sim808_buffer.put(ch);
+}
 
 /* @brief Write command `cmd` to the serial port `sr` and add newline '\n'.
  *
  * @param cmd Command to send.
  * @param sr mbed Serial object connected to SIM808_V2 module.
  */
-void sim808v2_send_cmd(const char * cmd, Serial * sr)
+void sim808v2_send_cmd(const char * cmd, BufferedSerial * sr)
 {
     sr -> puts(cmd);
     sr -> puts("\n");
-}
-
-/* @brief Clear global buffer.
- *
- * Clear global buffer by filling it with zeros and returning buffer index to
- * the starting position.
- */
-void sim808v2_clear_buffer(void)
-{
-    uint16_t i;
-    for(i = 0; i < BUFFER_SIZE; i++)
-    {
-        buffer[i] = 0x00;
-    }
-    buf_ind = 0;
-}
-
-/* @brief Prints buffer content. Used for printing SIM808_V2's response to
- * commands.
- *
- */
-void sim808v2_print_buffer(void)
-{
-    pc.printf("Response: %s\r\n", buffer);
-    sim808v2_clear_buffer(); 
-}
-
-/* @brief Make `num` of blinks by LED `green`.
- *
- * @param num Number of blinks
- */
-void n_blinks(int num){
-    for (int i = 0; i < num; i++) {
-        green = 1;
-        wait(0.2);
-        green = 0;
-        wait(0.2);
-    }
 }
 
 int setup_connection(TCPSocket * socket, char * ssid, char * seckey){
@@ -194,7 +160,11 @@ int main() {
         return -1;
     }
 
-    err = setup_connection(&socket, ssid, seckey);
+    if(WIFION){
+        err = setup_connection(&socket, ssid, seckey);
+    } else{
+        err = 0;
+    }
     
     if(err != 0) 
     {
@@ -207,14 +177,20 @@ int main() {
         num = send_string("@42;I;Ver:1a#", 13, &socket);
         DEBUG(("Sent %d bytes. \r\n", num));
 
-        sim808v2_print_buffer();
+        // Start session
+        if(WIFION){
+            num = send_string("@42;I;Ver:1a#", 13, &socket);
+            DEBUG(("Sent %d bytes. \r\n", num));
+        }
 
         while(1) { 
             if (mybutton == 0){
                 // Button Pressed
                 wait(0.1);
-                n_blinks(2);
-                num = send_string("@42;T;Button is pressed#", 24, &socket);
+                n_blinks(2, &green);
+                if(WIFION){
+                    num = send_string("@42;T;Button is pressed#", 24, &socket);
+                }
             }
             else{
                 sim808v2_send_cmd("AT+CGNSINF", &module);
@@ -232,12 +208,14 @@ int main() {
         }
     }
     
-    pc.printf("\r\nClosing Socket\r\n");
-    socket.close();
-    pc.printf("\r\nUnsecure Socket Test complete.\r\n");
-    printf ("Socket closed\n\r");
-    spwf.disconnect();
-    printf ("WIFI disconnected, exiting ...\n\r");
+    if(WIFION){
+        pc.printf("Closing Socket\r\n");
+        socket.close();
+        pc.printf("Unsecure Socket Test complete.\r\n");
+        printf ("Socket closed\n\r");
+        spwf.disconnect();
+        printf ("WIFI disconnected, exiting ...\n\r");
+    }
 
 }
 
